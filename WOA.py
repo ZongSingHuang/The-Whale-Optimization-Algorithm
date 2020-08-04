46977f30ed691645efd9201c0859a8cf71390e67
ZongSingHuang <58209813+ZongSingHuang@users.noreply.github.com>
2020-08-04 12:19:04 +0800

    2020.08.04

--- WOA.py
+++ WOA.py
@@ -11,7 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class WOA():
-    def __init__(self, fit_func, num_dim=30, num_particle=20, max_iter=500, update_when_iter=False,
+    def __init__(self, fit_func, update_with_iteration=True, num_dim=30, num_particle=20, max_iter=500, 
                  b=1, x_max=1, x_min=0, a_max=2, a_min=0, l_max=1, l_min=-1, a2_max=-1, a2_min=-2):
         self.fit_func = fit_func        
         self.num_dim = num_dim
@@ -26,8 +26,8 @@ class WOA():
         self.l_max = l_max
         self.l_min = l_min
         self.b = b
-        self.update_when_iter = update_when_iter
-
+        self.update_with_iteration = update_with_iteration
+        
         self._iter = 1
         self.gBest_X = None
         self.gBest_score = np.inf
@@ -40,6 +40,8 @@ class WOA():
         self.gBest_curve[0] = self.gBest_score.copy()
         
     def opt(self):
+        
+        
         while(self._iter<self.max_iter):
             a = self.a_max - (self.a_max-self.a_min)*(self._iter/self.max_iter)
             
@@ -50,8 +52,12 @@ class WOA():
                 A = 2*a*R1 - a
                 C = 2*R2
                 l = np.random.uniform()*(self.l_max-self.l_min) + self.l_min
-
-                if p<0.5:
+                
+                # case3-2. 改善速度
+                if p>0.5:
+                    D = np.abs(self.gBest_X - self.X[i, :])
+                    self.X[i, :] = D*np.exp(self.b*l)*np.cos(2*np.pi*l)+self.gBest_X                    
+                else:
                     if np.abs(A)<1:
                         D = np.abs(C*self.gBest_X - self.X[i, :])
                         self.X[i, :] = self.gBest_X - A*D
@@ -59,14 +65,10 @@ class WOA():
                         X_rand = self.X[np.random.randint(low=0, high=self.num_particle, size=self.num_dim), :]
                         X_rand = np.diag(X_rand).copy()
                         D = np.abs(C*X_rand - self.X[i, :])
-                        self.X[i, :] = self.gBest_X - A*D                
-                else:
-                    D = np.abs(self.gBest_X - self.X[i, :])
-                    self.X[i, :] = D*np.exp(self.b*l)*np.cos(2*np.pi*l)+self.gBest_X                    
+                        self.X[i, :] = self.gBest_X - A*D
 
-                        
-                if self.update_when_iter==False:      
-                    # case2-2. 每跑完一條就更新一次gBest
+            # case2-2. 每跑完一條就更新一次gBest
+                if self.update_with_iteration==False:
                     self.X[i, self.x_max < self.X[i, :]] = self.x_max[self.x_max < self.X[i, :]]
                     self.X[i, self.x_min > self.X[i, :]] = self.x_min[self.x_min > self.X[i, :]]
                     score = self.fit_func(self.X[i])
@@ -74,8 +76,8 @@ class WOA():
                         self.gBest_X = self.X[i, :].copy()
                         self.gBest_score = score.copy()
             
-            if self.update_when_iter==True:
-                # case2-1. 每跑完一代才更新gBest
+            # case2-1. 每跑完一代才更新gBests
+            if self.update_with_iteration==True:
                 bound_max = np.dot(np.ones(self.num_particle)[:, np.newaxis], self.x_max[np.newaxis, :])
                 bound_min = np.dot(np.ones(self.num_particle)[:, np.newaxis], self.x_min[np.newaxis, :])
                 self.X[bound_max < self.X] = bound_max[bound_max < self.X]
