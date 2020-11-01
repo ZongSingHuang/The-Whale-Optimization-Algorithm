14dbbc09a2e41d4417718fe0f1bc99a613ee1ea7
ZongSingHuang <58209813+ZongSingHuang@users.noreply.github.com>
2020-11-01 15:46:31 +0800

    2020.11.01

--- WOA.py
+++ WOA.py
@@ -4,19 +4,21 @@ Created on Thu Jul 16 21:59:58 2020
 
 @author: ZongSing_NB
 
-Main reference:http://www.alimirjalili.com/WOA.html
+Main reference:
+https://seyedalimirjalili.com/woa
+https://doi.org/10.1016/j.advengsoft.2016.01.008
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 class WOA():
-    def __init__(self, fit_func, strategy_obl=True, num_dim=30, num_particle=20, max_iter=500, 
+    def __init__(self, fit_func, num_dim=30, num_particle=20, max_iter=500, 
                  b=1, x_max=1, x_min=0, a_max=2, a_min=0, l_max=1, l_min=-1, a2_max=-1, a2_min=-2):
-        self.fit_func = fit_func        
+        self.fit_func = fit_func
         self.num_dim = num_dim
         self.num_particle = num_particle
-        self.max_iter = max_iter     
+        self.max_iter = max_iter
         self.x_max = x_max
         self.x_min = x_min
         self.a_max = a_max
@@ -26,13 +28,12 @@ class WOA():
         self.l_max = l_max
         self.l_min = l_min
         self.b = b
-        self.strategy_obl = strategy_obl
         
         self._iter = 1
         self.gBest_X = None
         self.gBest_score = np.inf
         self.gBest_curve = np.zeros(self.max_iter)
-        self.X = np.random.uniform(size=[self.num_particle, self.num_dim])*(self.x_max-self.x_min) + self.x_min
+        self.X = np.random.uniform(low=self.x_min, high=self.x_max, size=[self.num_particle, self.num_dim])
 
         score = self.fit_func(self.X)
         self.gBest_score = score.min().copy()
@@ -47,34 +48,31 @@ class WOA():
                 p = np.random.uniform()
                 R1 = np.random.uniform()
                 R2 = np.random.uniform()
-                A = 2*a*R1 - a
-                C = 2*R2
+                A = 2*a*R1 - a #(2.3)
+                C = 2*R2 # (2.4)
                 l = np.random.uniform()*(self.l_max-self.l_min) + self.l_min
                 
                 if p>0.5:
                     D = np.abs(self.gBest_X - self.X[i, :])
-                    self.X[i, :] = D*np.exp(self.b*l)*np.cos(2*np.pi*l)+self.gBest_X                    
+                    self.X[i, :] = D*np.exp(self.b*l)*np.cos(2*np.pi*l)+self.gBest_X # (2.5)
                 else:
                     if np.abs(A)<1:
-                        D = np.abs(C*self.gBest_X - self.X[i, :])
-                        self.X[i, :] = self.gBest_X - A*D
+                        D = np.abs(C*self.gBest_X - self.X[i, :]) # (2.1)
+                        self.X[i, :] = self.gBest_X - A*D # (2.2)
                     else:
                         X_rand = self.X[np.random.randint(low=0, high=self.num_particle, size=self.num_dim), :]
                         X_rand = np.diag(X_rand).copy()
-                        D = np.abs(C*X_rand - self.X[i, :])
-                        self.X[i, :] = X_rand - A*D
+                        D = np.abs(C*X_rand - self.X[i, :]) # (2.7)
+                        self.X[i, :] = X_rand - A*D # (2.8)
             
-            bound_max = np.dot(np.ones(self.num_particle)[:, np.newaxis], self.x_max[np.newaxis, :])
-            bound_min = np.dot(np.ones(self.num_particle)[:, np.newaxis], self.x_min[np.newaxis, :])
-            self.X[bound_max < self.X] = bound_max[bound_max < self.X]
-            self.X[bound_min > self.X] = bound_min[bound_min > self.X]
+            self.X = np.clip(self.X, self.x_min, self.x_max)
             
             score = self.fit_func(self.X)
             if np.min(score) < self.gBest_score:
                 self.gBest_X = self.X[score.argmin()].copy()
                 self.gBest_score = score.min().copy()
                 
-            self.gBest_curve[self._iter] = self.gBest_score.copy()    
+            self.gBest_curve[self._iter] = self.gBest_score.copy()
             self._iter = self._iter + 1
         
     def plot_curve(self):
@@ -83,5 +81,5 @@ class WOA():
         plt.plot(self.gBest_curve, label='loss')
         plt.grid()
         plt.legend()
-        plt.show()        
+        plt.show()
             
